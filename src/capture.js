a43cb354c20ecd8074de8805c2f4334db3d98c6f
Sieng, Socheat <socheat.sieng@cba.com.au>
2014-03-03 10:24:05 +1100

    Fix broken build

--- src/capture.js
+++ src/capture.js
@@ -48,7 +48,7 @@ function listen (appRoot, port, options) {
 
     var getFileName = function (root, req, type) {
         var fileName = combinePaths(req.url);
-        if (fileName === '/') {
+        if (fileName === path.sep) {
             fileName = 'root';
         }
         return path.resolve(root, util.format("%s-%s.%s", fileName.replace(/[\/=\?:&\\]/g, '_').replace(/^_/, ''), (new Date()).valueOf(), type.substring(0, 3)));
